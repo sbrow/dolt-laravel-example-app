16db3a3bc800ed19db585a94b009f8793cdc6ad4
Spencer Brower <brower.spencer@gmail.com>
2023-11-07 10:28:08 -0500

    fix: Used local install of dolt.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    dolt.url = "github:sbrow/dolt";
+    #dolt.url = "github:sbrow/dolt";
     #dolt.follows = "nixpkgs";
   };
 
@@ -19,7 +19,7 @@
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           caddy
-          doltPkg
+          #doltPkg
           foreman
           php
           phpPackages.composer
